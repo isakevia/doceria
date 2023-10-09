252ec92d1a85e582eaea80438fa256da22bc6fea
Isabelli Kevia da Silva <isabellisilva506@gmail.com>
2023-10-08 22:41:59 -0300

    arrumando

--- doceriapw/src/pages/Cardapio/index.tsx
+++ doceriapw/src/pages/Cardapio/index.tsx
@@ -1,12 +1,32 @@
 import styles from './Cardapio.module.scss';
-import logo from 'assets/logo.png'
+import logo from 'assets/logo.svg';
+import Buscador from '../Buscador';
+import { useState } from 'react';
+import Filtros from '../Filtros';
 
-export default function Cardapio(){
+export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null >(null);
     return (
-        <main>
-            <nav className={styles.menu}>Doceria
-            <img src={logo} alt="logo" />
-            </nav>
-        </main>
+    <main>
+        <nav className={styles.menu}>
+            <img src= {logo} alt="logo" />
+        </nav>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+                Os doces mais gostosos da região!
+            </div>
+        </header>
+
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca}/>
+
+            <div className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro}/>
+        </div>
+        </section>
+
+   </main>
     );
 }
